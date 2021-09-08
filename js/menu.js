f9840ba468fd3feeda5afa68e44b98e3196568be
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-09-08 06:06:11 +0100

    Fix mobile styling

---
+++ js/menu.js
@@ -0,0 +1,24 @@
+const menuBtn = document.querySelector('.hamburger-icon');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+// Handle click event on menu button
+menuBtn.addEventListener('click', () => {
+  const menuBtnClasses = menuBtn.classList;
+  menuBtnClasses.toggle('open');
+
+  const classMenu = mobileMenu.classList;
+  if (classMenu.contains('hide')) {
+    classMenu.replace('hide', 'show');
+    disableScroll();
+  } else if (classMenu.contains('show')) {
+    classMenu.replace('show', 'hide');
+    enableScroll();
+  }
+  // Handle click event on links from mobile menu;
+  const links = mobileMenu.querySelectorAll('a');
+  links.forEach((l) => l.addEventListener('click', () => {
+    menuBtn.classList.remove('open');
+    classMenu.replace('show', 'hide');
+    enableScroll();
+  }));
+});
